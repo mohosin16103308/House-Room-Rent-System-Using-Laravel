e3f3c0be5bdfc55a4cb10e5173fa6a709ede54ea
mohosin16103308 <hamza1610330816@gmail.com>
2020-01-02 00:46:17 +0600

    Dashboard Mastering

---
+++ resources/views/dashboard/inc/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+    <!--left-fixed -navigation-->
+    <aside class="sidebar-left">
+  <nav class="navbar navbar-inverse">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+        <h1><a class="navbar-brand" href="index.html"><span class="fa fa-area-chart"></span> Glance<span class="dashboard_text">Design dashboard</span></a></h1>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="sidebar-menu">
+          <li class="header">MAIN NAVIGATION</li>
+          <li class="treeview">
+            <a href="index.html">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            </a>
+          </li>
+          <li class="treeview">
+            <a href="#">
+            <i class="fa fa-laptop"></i>
+            <span>Components</span>
+            <i class="fa fa-angle-left pull-right"></i>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="grids.html"><i class="fa fa-angle-right"></i> Grids</a></li>
+              <li><a href="media.html"><i class="fa fa-angle-right"></i> Media Css</a></li>
+            </ul>
+          </li>
+
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+  </nav>
+</aside>
+</div>
+    <!--left-fixed -navigation-->
